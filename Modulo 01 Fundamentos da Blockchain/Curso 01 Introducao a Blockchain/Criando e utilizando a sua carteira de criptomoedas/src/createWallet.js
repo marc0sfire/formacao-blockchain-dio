f48e7d3a6f959b2bc864b482ad894368422de185
marc0sfire <101986454+marc0sfire@users.noreply.github.com>
2024-07-13 21:55:30 -0300

    minha createWallet.js
    criação de wallet do bitcoin na rede testnet do bitcoin

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -1,33 +1,35 @@
-//importando as dependencias
-const bip32 = require('bip32')
-const bip39 = require('bip39')
-const bitcoin = require('bitcoinjs-lib')
+// Importando as dependências
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
 
-//definir a rede
-//bitcoin - rede principal - mainnet
-//testnet - rede de teste - tesnet
-const network = bitcoin.networks.testnet
+// Definir a rede
+// bitcoin - rede principal - mainnet
+// testnet - rede de testes - testnet
+const network = bitcoin.networks.testnet;
 
-//derivação de carteiras HD
-const path = `m/49'/1'/0'/0` 
+// Carteira hierárquica determinística
+// Derivação de carteiras HD
+const path = "m/44'/1'/0'/0/0";
 
-//criando o mnemonic para a seed (palavras de senha)
-let mnemonic = bip39.generateMnemonic()
-const seed = bip39.mnemonicToSeedSync(mnemonic)
+// Criando as palavras mnemonic para a seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic();
+const seed = bip39.mnemonicToSeedSync(mnemonic);
 
-//criando a raiz da cartiera HD
-let root = bip32.fromSeed(seed, network)
+// Criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network);
 
-//criando uma conta - par pvt-pub keys
-let account = root.derivePath(path)
-let node = account.derive(0).derive(0)
+// Criando uma conta - par pvt-pub keys (chaves privadas e públicas)
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
 
+// Gerando endereço
 let btcAddress = bitcoin.payments.p2pkh({
     pubkey: node.publicKey,
     network: network,
-}).address
+}).address;
 
-console.log("Carteira gerada")
-console.log("Endereço: ", btcAddress)
-console.log("Chave privada:", node.toWIF())
-console.log("Seed:", mnemonic)
+console.log("Carteira gerada");
+console.log("Endereço:", btcAddress);
+console.log("Chave privada:", node.toWIF());
+console.log("Seed", mnemonic);
